bfdbd66796de11508d44b393668ecd5bfdd299f5
unknown <edgar.henao@labinfo.is.escuelaing.edu.co>
2022-09-12 14:06:55 -0500

    fixing heroku problem

--- src/main/java/edu/escuelaing/arep/sparkweb/AlphaAdvantageHttpStockService.java
+++ src/main/java/edu/escuelaing/arep/sparkweb/AlphaAdvantageHttpStockService.java
@@ -5,7 +5,7 @@ public class AlphaAdvantageHttpStockService extends HttpStockService{
     @Override
     public String getURL(String timeSeries, String stock) {
         // TODO Auto-generated method stub
-        return "https://www.alphavantage.co/query?function=TIME_SERIES_"+timeSeries+"&symbol="+stock+"&interval=5min&apikey=4NC2BOCH03RV7XZ0";
+        return "https://www.alphavantage.co/query?function=TIME_SERIES_"+timeSeries+"&symbol="+stock+"&interval=5min&apikey=976MWGKK4HB0UREA";
     }
 
     
